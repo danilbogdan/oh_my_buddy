effa64f8eff5b4ea98d4479c4fe7fd378fe83e10
bogdan.danilyuk <bogdan.danilyuk@eosda.com>
2025-02-01 20:06:28 +0100

    add FORCE_SCRIPT_NAME

--- main/settings/base.py
+++ main/settings/base.py
@@ -26,7 +26,7 @@ environ.Env.read_env(BASE_DIR / ".env")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str("SECRET_KEY")
-
+FORCE_SCRIPT_NAME = env.str("FORCE_SCRIPT_NAME", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=True)
 
