4b5dab380c4f79998d33c4d7b537490e6cba3cbd
bogdan.danilyuk <bogdan.danilyuk@eosda.com>
2025-03-12 18:30:08 +0100

    another optimization

--- apps/telegrambot/views.py
+++ apps/telegrambot/views.py
@@ -68,7 +68,8 @@ async def handle_update(request: HttpRequest, bot_id: int, user_id: int) -> None
     response = await agent.async_generate_response(message, update.message.chat.id, bot_model.id)
     await log_conversation(bot_model, update.message.chat.id, bot_model.name, response)
     try:
-        await update.message.reply_text(escape_markdown(response, 2), parse_mode=bot_model.parse_mode)
+        response = response.replace("!", escape_markdown(response, 2))
+        await update.message.reply_text(response, parse_mode=bot_model.parse_mode)
     except tgerror.BadRequest as e:
         logger.info(f"Cant parse as {bot_model.parse_mode}, try simple response... ", exc_info=e)
         await update.message.reply_text(response)
